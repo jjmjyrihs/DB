5f875c7db42dd6ab72ab489cfe991aa4333906ec
jjmjyrihs <1104137128@gm.kuas.edu.tw>
2017-06-01 03:51:52 +0800

    Home_View_Okay

---
+++ DB/DB/Controllers/InquireController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DB.Controllers
+{
+    public class InquireController : Controller
+    {
+        // GET: Inquire
+        public ActionResult Index(string BookName)
+        {
+
+            return View();
+        }
+    }
+}
