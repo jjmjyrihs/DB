5cfd1368c59d07b9790d17161967a003cbd4564b
jjmjyrihs <1104137128@gm.kuas.edu.tw>
2017-06-01 12:28:37 +0800

    Add Registered

---
+++ DB/DB/Controllers/RegisteredController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DB.Controllers
+{
+    public class RegisteredController : Controller
+    {
+        // GET: Registered
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Insert(string one, string two)
+        {
+            ViewBag.one = one;
+            ViewBag.two = two;
+            return View();
+        }
+    }
+}
